a8f10763b8b80fd88997ffa38d397ba4fcb8ba70
bevinart <github.com.retaining261@passmail.net>
2025-01-01 22:08:31 -0500

    Making base files for project

---
+++ include/bittorrent.h
@@ -0,0 +1,14 @@
+#ifndef BITTORRENT_CLI
+#define BITTORRENT_CLI
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+#include <bittorrent.h>
+
+int main(int argc, char * argv[]) {
+    return 0;
+}
